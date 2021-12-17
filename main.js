98847973f48230ad1392997827753afa797c714e
Sikandar Bhide <93428946+SikandarJODD@users.noreply.github.com>
2021-12-17 23:18:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+
+// let playSound = () => new Audio("/storage/TumMile.mp3").play();
+// let Sound = () => new Audio("/storage/TumMile.mp3").pause();
+var audio = document.getElementById('audio-1');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+function playpause() {
+    if (count == 0) {
+        count = 1;
+        audio.play();
+        playPauseBTN.innerHTML = "Pause &#9208;";
+    }
+    else {
+        count = 0;
+        audio.pause();
+        playPauseBTN.innerHTML = "Play &#9211;";
+    }
+}
+function stop() {
+    playpause();
+    audio.pause();
+    audio.currentTime = 0;
+    playPauseBTN.innerHTML = "Play &#9211;";
+}
+// Song 2 
+var audio1 = document.getElementById('audio-2');
+var playPauseBTN1 = document.getElementById('playPauseBTN2');
+var count1 = 0;
+function playpause1() {
+    if (count1 == 0) {
+        count1 = 1;
+        audio1.play();
+        playPauseBTN1.innerHTML = "Pause &#9208;";
+    }
+    else {
+        count1 = 0;1
+        audio1.pause();
+        playPauseBTN1.innerHTML = "Play &#9211;";
+    }
+}
+function stop1() {
+    playpause1();
+    audio1.pause();
+    audio1.currentTime = 0;
+    playPauseBTN1.innerHTML = "Play &#9211;";
+}
+// Song 3
+var audio3 = document.getElementById('audio-3');
+var playPauseBTN3 = document.getElementById('playPauseBTN3');
+function playpause() {
+    if (count == 0) {
+        count = 1;
+        audio3.play();
+        playPauseBTN3.innerHTML = "Pause &#9208;";
+    }
+    else {
+        count = 0;
+        audio3.pause();
+        playPauseBTN3.innerHTML = "Play &#9211;";
+    }
+}
+function stop() {
+    playpause();
+    audio3.pause();
+    audio3.currentTime = 0;
+    playPauseBTN3.innerHTML = "Play &#9211;";
+}
+// Song 4
+var audio4 = document.getElementById('audio-4');
+var playPauseBTN4 = document.getElementById('playPauseBTN4');
+var count = 0;
+function playpause() {
+    if (count == 0) {
+        count = 1;
+        audio4.play();
+        playPauseBTN4.innerHTML = "Pause &#9208;";
+    }
+    else {
+        count = 0;
+        audio4.pause();
+        playPauseBTN4.innerHTML = "Play &#9211;";
+    }
+}
+function stop() {
+    playpause();
+    audio4.pause();
+    audio4.currentTime = 0;
+    playPauseBTN4.innerHTML = "Play &#9211;";
+}
+// Song 5
+var audio5 = document.getElementById('audio-5');
+var playPauseBTN5 = document.getElementById('playPauseBTN5');
+var count = 0;
+function playpause() {
+    if (count == 0) {
+        count = 1;
+        audio5.play();
+        playPauseBTN5.innerHTML = "Pause &#9208;";
+    }
+    else {
+        count = 0;
+        audio5.pause();
+        playPauseBTN5.innerHTML = "Play &#9211;";
+    }
+}
+function stop() {
+    playpause();
+    audio5.pause();
+    audio5.currentTime = 0;
+    playPauseBTN5.innerHTML = "Play &#9211;";
+}
